868a691ac18dc82fb98044795e8dae758dd48f50
BlueWe11s <lshepetr@gmail.com>
2025-02-23 18:52:02 +0300

    1st review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,81 @@
+import re
+
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from drf_extra_fields.fields import Base64ImageField
+from djoser.serializers import UserSerializer as DjoserSerializer
 from rest_framework import serializers
 
 from recipes.models import (FavoriteRecipe, Ingredient, Recipe,
                             RecipeIngredient, ShoppingCart, Tags)
 from user.models import Follow, Users
-from user.serializers import UserSerializer
+from api.serializers import UserSerializer
 
 User = get_user_model()
+
+
+from user.models import Follow
+
+class UserSerializer(DjoserSerializer):
+    """
+    Сериализатор для пользователя
+    """
+
+    is_subscribed = serializers.SerializerMethodField(default=False)
+
+    class Meta(DjoserSerializer.Meta):
+        model = User
+        fields = (
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "avatar",
+            "is_subscribed",
+        )
+        read_only_fields = ("id",)
+
+    def validate_username(value):
+        pattern = r"^[\w.@+-]+\Z"
+        if value == "me":
+            raise ValidationError(
+                "Вы не можете выбрать никнейм 'me', "
+                "выберите другой никнейм."
+            )
+        if not re.match(pattern, value):
+            invalid_chars = re.sub(pattern, "", value)
+            raise ValidationError(
+                f"Введите корректный юзернейм."
+                f"Эти символы недопустимы: {invalid_chars}"
+            )
+        return value
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get("request")
+        return Follow.objects.filter(
+            user=request.user.id, author=obj.id
+        ).exists()
+
+
+class UserAvatarSerializer(serializers.Serializer):
+    """
+    Сериализатор аватара польтзователя
+    """
+
+    avatar = Base64ImageField(required=True)
+
+    def update(self, instance, validated_data):
+        if not validated_data.get("avatar"):
+            raise serializers.ValidationError("Поле пусто")
+        instance.avatar = validated_data.get("avatar")
+        instance.save()
+        return instance
+
+    class Meta:
+        model = User
+        fields = ("avatar",)
+
 
 
 class TagsSerializer(serializers.ModelSerializer):
