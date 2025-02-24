67a1bfd715510d678319e0aac9dacfc87dfc70fd
BlueWells <lshepetr@gmail.com>
2025-02-24 09:25:19 +0300

    Add primal MVC

--- backend/api/recipes/serializers.py
+++ backend/api/recipes/serializers.py
@@ -1,77 +1,12 @@
-import re
-
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from drf_extra_fields.fields import Base64ImageField
-from djoser.serializers import UserSerializer as DjoserSerializer
 from rest_framework import serializers
 
 from recipes.models import (FavoriteRecipe, Ingredient, Recipe,
                             RecipeIngredient, ShoppingCart, Tags)
-from user.models import Follow, Users
+from user.serializers import UserSerializer
 
 User = get_user_model()
-
-
-class UserSerializer(DjoserSerializer):
-    """
-    Сериализатор для пользователя
-    """
-
-    is_subscribed = serializers.SerializerMethodField(default=False)
-
-    class Meta(DjoserSerializer.Meta):
-        model = User
-        fields = (
-            "id",
-            "email",
-            "username",
-            "first_name",
-            "last_name",
-            "avatar",
-            "is_subscribed",
-        )
-        read_only_fields = ("id",)
-
-    def validate_username(value):
-        pattern = r"^[\w.@+-]+\Z"
-        if value == "me":
-            raise ValidationError(
-                "Вы не можете выбрать никнейм 'me', "
-                "выберите другой никнейм."
-            )
-        if not re.match(pattern, value):
-            invalid_chars = re.sub(pattern, "", value)
-            raise ValidationError(
-                f"Введите корректный юзернейм."
-                f"Эти символы недопустимы: {invalid_chars}"
-            )
-        return value
-
-    def get_is_subscribed(self, obj):
-        request = self.context.get("request")
-        return Follow.objects.filter(
-            user=request.user.id, author=obj.id
-        ).exists()
-
-
-class UserAvatarSerializer(serializers.Serializer):
-    """
-    Сериализатор аватара польтзователя
-    """
-
-    avatar = Base64ImageField(required=True)
-
-    def update(self, instance, validated_data):
-        if not validated_data.get("avatar"):
-            raise serializers.ValidationError("Поле пусто")
-        instance.avatar = validated_data.get("avatar")
-        instance.save()
-        return instance
-
-    class Meta:
-        model = User
-        fields = ("avatar",)
 
 
 class TagsSerializer(serializers.ModelSerializer):
@@ -349,78 +284,3 @@ class CartsSerializer(serializers.ModelSerializer):
             "image",
             "cooking_time",
         )
-
-
-class SubscribingSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор подписчиков
-    """
-
-    is_subscribed = serializers.SerializerMethodField(default=False)
-    recipes = serializers.SerializerMethodField(method_name="get_recipes")
-    recipes_count = serializers.ReadOnlyField(default=0)
-
-    class Meta:
-        model = Users
-        fields = (
-            "id",
-            "email",
-            "username",
-            "first_name",
-            "last_name",
-            "avatar",
-            "is_subscribed",
-            "recipes",
-            "recipes_count",
-        )
-        read_only_fields = ("id",)
-
-    def get_is_subscribed(self, obj):
-        return Follow.objects.filter(
-            user=self.context["request"].user, author=obj
-        ).exists()
-
-    def get_recipes(self, obj):
-        request = self.context["request"]
-        queryset = obj.recipes.all()
-        limit = request.query_params.get("recipes_limit")
-        if limit:
-            try:
-                queryset = queryset[: int(limit)]
-            except (TypeError, ValueError):
-                pass
-        return CartsSerializer(
-            queryset,
-            many=True,
-            context={"request": request},
-        ).data
-
-    def get_recipes_count(self, obj):
-        return obj.recipes.count()
-
-
-class SubscribeSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор подписок
-    """
-
-    class Meta:
-        model = Follow
-        fields = ("user", "author")
-
-    def validate(self, data):
-        user = data["user"]
-        author = data["author"]
-        if user == author:
-            raise serializers.ValidationError(
-                "Вы не можете подписаться на себя"
-            )
-        if Follow.objects.filter(user=user, author=author).exists():
-            raise serializers.ValidationError("Вы уже подписаны")
-        return data
-
-    def to_representation(self, instance):
-        return SubscribingSerializer(
-            instance.author,
-            context=self.context,
-        ).data
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
-from user.views import UsersViewSet
+from api.recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
+from api.user.views import UsersViewSet
 
 router = DefaultRouter()
 router.register('users', UsersViewSet, basename='users')
